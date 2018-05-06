15c6362384471c9b3eee0ce7253cb587de5a9b4a
Pietro Rea <pietorea@gmail.com>
2018-05-06 09:21:26 -0400

    Rounded corners for blog photo

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -24,6 +24,7 @@ class Bio extends React.Component {
             marginBottom: 0,
             width: rhythm(2),
             height: rhythm(2),
+            borderRadius: rhythm(1),
           }}
         />
         <p>
