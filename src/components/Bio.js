0f65f618d74c66e770b132e27fef33fa768bcd81
Pietro Rea <pietorea@gmail.com>
2018-05-06 10:49:10 -0400

    Changing alt text for photo to my name

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -18,7 +18,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Pietro Rea`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
