ed2884b13fc33c0896ac416e28948ae12e5d0b0d
Pietro Rea <pietro@sweetpeamobile.com>
2020-07-15 15:26:20 -0400

    Removing Typography theme dependencies and using system fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,15 +1,79 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
 
-Wordpress2016.overrideThemeStyles = () => ({
-  'a.gatsby-resp-image-link': {
-    boxShadow: 'none',
-  },
+
+const typography = Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.75,
+  scaleRatio: 5 / 2,
+  googleFonts: [],
+  headerFontFamily: ['-apple-system', 'system-ui', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['-apple-system', 'system-ui', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyColor: "hsla(0,0%,0%,0.9)",
+  headerWeight: 900,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+    blockquote: {
+      ...scale(1 / 5),
+      color: 'hsla(0,0%,0%,0.59)',
+      fontStyle: "italic",
+      paddingLeft: rhythm(13 / 16),
+      marginLeft: rhythm(-1),
+      borderLeft: `${rhythm(3 / 16)} solid hsla(0,0%,0%,0.9)`,
+    },
+    "blockquote > :last-child": {
+      marginBottom: 0,
+    },
+    "blockquote cite": {
+      ...adjustFontSizeTo(options.baseFontSize),
+      color: options.bodyColor,
+      fontWeight: options.bodyWeight,
+    },
+    "blockquote cite:before": {
+      content: '"— "',
+    },
+    ul: {
+      listStyle: "disc",
+    },
+    "ul,ol": {
+      marginLeft: 0,
+    },
+    ['@media only screen and (max-width:480px)']: {
+      "ul,ol": {
+        marginLeft: rhythm(1),
+      },
+      blockquote: {
+        marginLeft: rhythm(-3 / 4),
+        marginRight: 0,
+        paddingLeft: rhythm(9 / 16),
+      },
+    },
+    "h1,h2,h3,h4,h5,h6": {
+      marginTop: rhythm(2),
+    },
+    h4: {
+      letterSpacing: "0.140625em",
+      textTransform: "uppercase",
+    },
+    h6: {
+      fontStyle: "italic",
+    },
+    a: {
+      boxShadow: "0 1px 0 0 currentColor",
+      color: "#007acc",
+      textDecoration: "none",
+    },
+    "a:hover,a:active": {
+      boxShadow: "none",
+    },
+    "mark,ins": {
+      background: "#007acc",
+      color: "white",
+      padding: `${rhythm(1 / 16)} ${rhythm(1 / 8)}`,
+      textDecoration: "none",
+    },
+  }),
 })
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
