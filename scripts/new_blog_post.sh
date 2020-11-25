179fddbe564fce95ed152474de13640e3cbb2cf2
Pietro Rea <pietro@sweetpeamobile.com>
2020-11-24 23:07:29 -0500

    new_blog_post.sh tweaks to be able to run from any directory.

--- scripts/new_blog_post.sh
+++ scripts/new_blog_post.sh
@@ -3,7 +3,16 @@
 echo "Enter blog post name:"
 read blog_post_name
 
-cd ../src/content
+script_dir=$(cd `dirname $0` && pwd)
+dir_destination="${script_dir}/../src/content"
+
+if [[ -d $dir_destination ]]
+then
+  cd $dir_destination
+else
+  echo "$dir_destination does not exit."
+  exit 1
+fi
 
 lower_case_name=`echo ${blog_post_name} | awk '{print tolower($0)}'`
 kebab_case_name=`echo ${lower_case_name} | sed 's/ /-/g'`
@@ -12,12 +21,13 @@ date_path=`date +"/%Y/%m/%d"`
 
 file="${date_dashes}-${kebab_case_name}.md"
 
-if [[ -f $file ]];then
-    echo "$file already exists."
-    exit 1
+if [[ -f $file ]]
+then
+  echo "$file already exists."
+  exit 1
 else
-    touch $file
-    echo "Creating ${file}"
+  touch $file
+  echo "Creating ${file}"
 fi
 
 frontmatter="
